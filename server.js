457862301abd32fdfca49ae5b0fff0eef84c149b
Sharon Lee <ssmlee10@gmail.com>
2025-07-21 13:17:21 -0700

    install mongoose, dotenv

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI);
+
+// connect to MongoDB dogs
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}`);
+})
 
 // GET /
 app.get("/", async (req, res) => {
